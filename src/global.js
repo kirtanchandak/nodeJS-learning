656677cc46bc2536bf7b744ec185bfe867da14dc
kirtanchandak <themarketingkirtan@gmail.com>
2023-01-26 00:11:37 +0530

    globaljs

---
+++ src/global.js
@@ -0,0 +1,7 @@
+// Global objects are available in Node.js and Browser.
+setTimeout(() => {
+  console.log("I will come after 3 seconds");
+}, 3000);
+
+console.log(__dirname);
+console.log(__filename);
